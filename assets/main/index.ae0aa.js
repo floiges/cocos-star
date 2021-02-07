8c367e3e2dfae8109b2561a07a0c6a9eea336408
yadong <liuyadong@yunkezan.com.cn>
2021-02-07 15:45:02 +0800

    feat: deploy

---
+++ assets/main/index.ae0aa.js
@@ -0,0 +1,307 @@
+window.__require = function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var b = o.split("/");
+        b = b[b.length - 1];
+        if (!t[b]) {
+          var a = "function" == typeof __require && __require;
+          if (!u && a) return a(b, !0);
+          if (i) return i(b, !0);
+          throw new Error("Cannot find module '" + o + "'");
+        }
+        o = b;
+      }
+      var f = n[o] = {
+        exports: {}
+      };
+      t[o][0].call(f.exports, function(e) {
+        var n = t[o][1][e];
+        return s(n || e);
+      }, f, f.exports, e, t, n, r);
+    }
+    return n[o].exports;
+  }
+  var i = "function" == typeof __require && __require;
+  for (var o = 0; o < r.length; o++) s(r[o]);
+  return s;
+}({
+  Game: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "61a93mGGcNCJ5cluiSIXhDq", "Game");
+    "use strict";
+    var __extends = this && this.__extends || function() {
+      var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf || {
+          __proto__: []
+        } instanceof Array && function(d, b) {
+          d.__proto__ = b;
+        } || function(d, b) {
+          for (var p in b) Object.prototype.hasOwnProperty.call(b, p) && (d[p] = b[p]);
+        };
+        return extendStatics(d, b);
+      };
+      return function(d, b) {
+        extendStatics(d, b);
+        function __() {
+          this.constructor = d;
+        }
+        d.prototype = null === b ? Object.create(b) : (__.prototype = b.prototype, new __());
+      };
+    }();
+    var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+      var c = arguments.length, r = c < 3 ? target : null === desc ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+      if ("object" === typeof Reflect && "function" === typeof Reflect.decorate) r = Reflect.decorate(decorators, target, key, desc); else for (var i = decorators.length - 1; i >= 0; i--) (d = decorators[i]) && (r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r);
+      return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+    var Game = function(_super) {
+      __extends(Game, _super);
+      function Game() {
+        var _this = null !== _super && _super.apply(this, arguments) || this;
+        _this.starPrefab = null;
+        _this.maxStarDuration = 0;
+        _this.minStarDuration = 0;
+        _this.ground = null;
+        _this.player = null;
+        _this.scoreDisplay = null;
+        _this.scoreAudio = null;
+        _this.groundY = 0;
+        _this.score = 0;
+        _this.timer = 0;
+        _this.startDuration = 0;
+        return _this;
+      }
+      Game.prototype.onLoad = function() {
+        this.groundY = this.ground.y + this.ground.height / 2;
+        this.timer = 0;
+        this.spawnNewStar();
+      };
+      Game.prototype.start = function() {};
+      Game.prototype.update = function(dt) {
+        if (this.timer > this.startDuration) {
+          this.gameOver();
+          return;
+        }
+        this.timer += dt;
+      };
+      Game.prototype.spawnNewStar = function() {
+        var newStar = cc.instantiate(this.starPrefab);
+        this.node.addChild(newStar);
+        newStar.setPosition(this.getNewStarPosition());
+        newStar.getComponent("Star").game = this;
+        this.startDuration = this.minStarDuration + Math.random() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
+      };
+      Game.prototype.getNewStarPosition = function() {
+        var randX = 0;
+        var randY = this.groundY + Math.random() * this.player.getComponent("Player").jumpHeight + 50;
+        var maxX = this.node.width / 2;
+        randX = 2 * (Math.random() - .5) * maxX;
+        return cc.v2(randX, randY);
+      };
+      Game.prototype.gainScore = function() {
+        this.score += 1;
+        this.scoreDisplay.string = "Score: " + this.score;
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+      };
+      Game.prototype.gameOver = function() {
+        this.player.stopAllActions();
+        cc.director.loadScene("game");
+      };
+      __decorate([ property(cc.Prefab) ], Game.prototype, "starPrefab", void 0);
+      __decorate([ property ], Game.prototype, "maxStarDuration", void 0);
+      __decorate([ property ], Game.prototype, "minStarDuration", void 0);
+      __decorate([ property(cc.Node) ], Game.prototype, "ground", void 0);
+      __decorate([ property(cc.Node) ], Game.prototype, "player", void 0);
+      __decorate([ property(cc.Label) ], Game.prototype, "scoreDisplay", void 0);
+      __decorate([ property(cc.AudioClip) ], Game.prototype, "scoreAudio", void 0);
+      Game = __decorate([ ccclass ], Game);
+      return Game;
+    }(cc.Component);
+    exports.default = Game;
+    cc._RF.pop();
+  }, {} ],
+  Player: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "21ca9/gsxRLFKqhj5ZZsmOe", "Player");
+    "use strict";
+    var __extends = this && this.__extends || function() {
+      var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf || {
+          __proto__: []
+        } instanceof Array && function(d, b) {
+          d.__proto__ = b;
+        } || function(d, b) {
+          for (var p in b) Object.prototype.hasOwnProperty.call(b, p) && (d[p] = b[p]);
+        };
+        return extendStatics(d, b);
+      };
+      return function(d, b) {
+        extendStatics(d, b);
+        function __() {
+          this.constructor = d;
+        }
+        d.prototype = null === b ? Object.create(b) : (__.prototype = b.prototype, new __());
+      };
+    }();
+    var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+      var c = arguments.length, r = c < 3 ? target : null === desc ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+      if ("object" === typeof Reflect && "function" === typeof Reflect.decorate) r = Reflect.decorate(decorators, target, key, desc); else for (var i = decorators.length - 1; i >= 0; i--) (d = decorators[i]) && (r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r);
+      return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+    var Player = function(_super) {
+      __extends(Player, _super);
+      function Player() {
+        var _this = null !== _super && _super.apply(this, arguments) || this;
+        _this.label = null;
+        _this.jumpHeight = 0;
+        _this.jumpDuration = 0;
+        _this.maxMoveSpeed = 0;
+        _this.accel = 0;
+        _this.jumpAudio = null;
+        _this.accLeft = false;
+        _this.accRight = false;
+        _this.xSpeed = 0;
+        return _this;
+      }
+      Player.prototype.onLoad = function() {
+        var jumpAction = this.runJumpAction();
+        cc.tween(this.node).then(jumpAction).start();
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+      };
+      Player.prototype.start = function() {};
+      Player.prototype.update = function(dt) {
+        this.accLeft ? this.xSpeed -= this.accel * dt : this.accRight && (this.xSpeed += this.accel * dt);
+        Math.abs(this.xSpeed) > this.maxMoveSpeed && (this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed));
+        this.node.x += this.xSpeed * dt;
+      };
+      Player.prototype.onDestroy = function() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+      };
+      Player.prototype.runJumpAction = function() {
+        var jumpUp = cc.tween().by(this.jumpDuration, {
+          y: this.jumpHeight
+        }, {
+          easing: "sineOut"
+        });
+        var jumpDown = cc.tween().by(this.jumpDuration, {
+          y: -this.jumpHeight
+        }, {
+          easing: "sineIn"
+        });
+        var tween = cc.tween().sequence(jumpUp, jumpDown).call(this.playJumpSound, this);
+        return cc.tween().repeatForever(tween);
+      };
+      Player.prototype.onKeyDown = function(event) {
+        switch (event.keyCode) {
+         case cc.macro.KEY.a:
+          this.accLeft = true;
+          break;
+
+         case cc.macro.KEY.d:
+          this.accRight = true;
+        }
+      };
+      Player.prototype.onKeyUp = function(event) {
+        switch (event.keyCode) {
+         case cc.macro.KEY.a:
+          this.accLeft = false;
+          break;
+
+         case cc.macro.KEY.d:
+          this.accRight = false;
+        }
+      };
+      Player.prototype.playJumpSound = function() {
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+      };
+      __decorate([ property(cc.Label) ], Player.prototype, "label", void 0);
+      __decorate([ property ], Player.prototype, "jumpHeight", void 0);
+      __decorate([ property ], Player.prototype, "jumpDuration", void 0);
+      __decorate([ property ], Player.prototype, "maxMoveSpeed", void 0);
+      __decorate([ property ], Player.prototype, "accel", void 0);
+      __decorate([ property(cc.AudioClip) ], Player.prototype, "jumpAudio", void 0);
+      Player = __decorate([ ccclass ], Player);
+      return Player;
+    }(cc.Component);
+    exports.default = Player;
+    cc._RF.pop();
+  }, {} ],
+  Star: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "cd8988ZyedBlJYPSVN4txdK", "Star");
+    "use strict";
+    var __extends = this && this.__extends || function() {
+      var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf || {
+          __proto__: []
+        } instanceof Array && function(d, b) {
+          d.__proto__ = b;
+        } || function(d, b) {
+          for (var p in b) Object.prototype.hasOwnProperty.call(b, p) && (d[p] = b[p]);
+        };
+        return extendStatics(d, b);
+      };
+      return function(d, b) {
+        extendStatics(d, b);
+        function __() {
+          this.constructor = d;
+        }
+        d.prototype = null === b ? Object.create(b) : (__.prototype = b.prototype, new __());
+      };
+    }();
+    var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+      var c = arguments.length, r = c < 3 ? target : null === desc ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+      if ("object" === typeof Reflect && "function" === typeof Reflect.decorate) r = Reflect.decorate(decorators, target, key, desc); else for (var i = decorators.length - 1; i >= 0; i--) (d = decorators[i]) && (r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r);
+      return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+    var Star = function(_super) {
+      __extends(Star, _super);
+      function Star() {
+        var _this = null !== _super && _super.apply(this, arguments) || this;
+        _this.pickRadius = 0;
+        _this.game = null;
+        return _this;
+      }
+      Star.prototype.start = function() {};
+      Star.prototype.update = function(dt) {
+        if (this.getPlayerDistance() < this.pickRadius) {
+          this.onPicked();
+          return;
+        }
+        var opacityRatio = 1 - this.game.timer / this.game.startDuration;
+        var minOpacity = 50;
+        this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
+      };
+      Star.prototype.getPlayerDistance = function() {
+        var playerPos = this.game.player.position;
+        var dist = this.node.position.sub(playerPos).mag();
+        return dist;
+      };
+      Star.prototype.onPicked = function() {
+        this.game.spawnNewStar();
+        this.game.gainScore();
+        this.node.destroy();
+      };
+      __decorate([ property ], Star.prototype, "pickRadius", void 0);
+      Star = __decorate([ ccclass ], Star);
+      return Star;
+    }(cc.Component);
+    exports.default = Star;
+    cc._RF.pop();
+  }, {} ]
+}, {}, [ "Game", "Player", "Star" ]);
---
+++ src/settings.7e291.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/game.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "a8581",
+        main: "ae0aa"
+    }
+};
